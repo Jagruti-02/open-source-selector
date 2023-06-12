cb6fe88071872c710055ff00882fd321ae9f5394
Jagruti <jagrutirkm@gmail.com>
2023-06-12 17:30:38 +0530

    ui update

--- src/components/MainPage.jsx
+++ src/components/MainPage.jsx
@@ -137,7 +137,7 @@ function MainPage() {
               <div className="flex rounded w-full">
                 <input
                   type="text"
-                  className="block w-full px-16 py-2 text-blue-700 bg-white border rounded-md focus:border-blue-400 focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40"
+                  className="block w-full px-16 py-3 text-blue-700 bg-white border rounded-md focus:border-blue-400 focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40 mt-10"
                   placeholder="Search by name..."
                   value={name}
                   onChange={(e) => setName(e.target.value)}
@@ -147,15 +147,15 @@ function MainPage() {
             <div className="flex rounded w-full">
                 <input
                   type="text"
-                  className="block w-full px-16 py-2 text-blue-700 bg-white border rounded-md focus:border-blue-400 focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40"
-                  placeholder="Search by tags..."
+                  className="block w-full px-16 py-3 text-blue-700 bg-white border rounded-md focus:border-blue-400 focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40 mb-10"
+                  placeholder="Search tags/keywords"
                   value={tags}
                   onChange={(e) => setTags(e.target.value)}
                 />
                 
               </div>
 
-            <div className="flex items-center">
+            {/* <div className="flex items-center">
                 <input
                   type="text"
                   className="block w-full px-16 py-2 text-blue-700 bg-white border rounded-md focus:border-blue-400 focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40"
@@ -164,7 +164,7 @@ function MainPage() {
                   onChange={(e) => setDesc(e.target.value)}
                 />
                 
-            </div>
+            </div> */}
             <button className=" px-4 pt-2 text-white bg-blue-600 border-l rounded " onClick={handleSearch}>
                   Search
                 </button>
